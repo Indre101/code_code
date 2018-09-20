b1080baf1620ab44a483d39fba77cc9f40ad4b15
Indre101 <zygaityte.indre@gmail.com>
2018-09-20 20:47:51 +0200

    last update

--- test4.py
+++ test4.py
@@ -36,3 +36,18 @@ def get_average(student):
 
 
 print get_average(alice)
+
+
+def get_letter_grade(score):
+	if score >= 90:
+    return "A"
+  elif score >= 80:
+    return "B"
+  elif score >= 70:
+    return "C"
+  elif score >= 60:
+    return "D"
+  else:
+    return "F"
+    
+print get_letter_grade(get_average(lloyd))
