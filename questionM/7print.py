116f6c30d6482d1a4394c671fa5a65440d85e055
Indre Zygaityte <indrezygaityte@Indres-MacBook-Pro.local>
2018-10-18 14:36:29 +0200

    fixed triangle code

---
+++ questionM/7print.py
@@ -0,0 +1,9 @@
+def main():
+    i= input("Number:")
+    print (f"Hello {i}")
+
+main()
+x= 3
+y=2
+print(f"{ x // y }") 
+print(x/y)
--- trikampiai.py
+++ trikampiai.py
@@ -1,5 +1,7 @@
-import math
-print "WHAT TYPE OF TRIANGE CALCULATION"
+from math import degrees
+from math import acos
+
+print ("WHAT TYPE OF TRIANGE CALCULATION")
 print ("BEGIN")
 
 
@@ -7,29 +9,19 @@ a = float(input("What is the length of first line: "))
 b = float(input("What is the length of first line: "))
 c = float(input("What is the length of first line: "))
 
-
-# def zygis(a, b, c):
-# return (a**2+b**2 - c**2) / (2 * a*b)
-
-
 if a > 0 and b > 0 and c > 0 and a <= b + c:
     a_kampas = (((a**2+b**2 - c**2) / (2 * a*b)))
     b_kampas = (((b**2+c**2 - a**2) / (2 * c*b)))
     c_kampas = (((a**2+c**2 - b**2) / (2 * a*c)))
 
-    a_laipsnis = math.degrees(math.acos(a_kampas))
-    b_laipsnis = math.degrees(math.acos(b_kampas))
-    c_laipsnis = math.degrees(math.acos(c_kampas))
+    a_laipsnis = degrees(acos(a_kampas))
+    b_laipsnis = degrees(acos(b_kampas))
+    c_laipsnis = degrees(acos(c_kampas))
 
 if a_laipsnis == 90 or b_laipsnis == 90 or c_laipsnis == 90:
-    print "The degrees of the corners are %i, %i and %i, so the triagle is rectangle" % (
-        a_laipsnis, b_laipsnis, c_laipsnis)
+    print (f"The degrees of the corners are {a_laipsnis:.2f}, {b_laipsnis:.2f} and {c_laipsnis:.2f}, so the triagle is rectangle") 
 elif a_laipsnis > 90 or b_laipsnis > 90 or c_laipsnis > 90:
-    print "The degrees of the corners are %i, %i and %i, so the triagle is obtuse" % (
-        a_laipsnis, b_laipsnis, c_laipsnis)
+    print (f"The degrees of the corners are {a_laipsnis:.2f}, {b_laipsnis:.2f} and {c_laipsnis:.2f}, so the triagle is obtuse")
 else:
-    print "The degrees of the corners are %i, %i and %i, so the triagle is acute" % (
-        a_laipsnis, b_laipsnis, c_laipsnis)
-
-
+    print (f"The degrees of the corners are {a_laipsnis:.2f}, {b_laipsnis:.2f} and {c_laipsnis:.2f}, so the triagle is acute" )
 # print "The length of lines can't be negative number or lower than 0"
