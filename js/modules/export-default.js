1969a1bccbeb345fc1878ba3e6e4bc717b6736f7
Indre Zygaityte <indrezygaityte@Indres-MacBook-Pro.local>
2019-01-08 18:56:34 +0100

    export default module method

---
+++ js/modules/export-default.js
@@ -0,0 +1,14 @@
+const Airplane = {};
+
+
+Airplane.availableAirplanes = [];
+Airplane.availableAirplanes.push({
+  name: 'AeroJet',
+  fuelCapacity: 800
+});
+Airplane.availableAirplanes.push({
+  name: 'SkyJet',
+  fuelCapacity: 500
+});
+
+export default Airplane;
