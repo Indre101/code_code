72d74a1e81585040f91635ca3e15e381034f56fc
Indre Zygaityte <zygaityte.indre@gmail.com>
2019-01-14 21:39:55 +0100

    finishes first promise function for laundry

--- js/promise/promise-practise/depending-promises-cleaning.js
+++ js/promise/promise-practise/depending-promises-cleaning.js
@@ -1,5 +1,9 @@
-let laudry = function () {
-  return new Promise(function (resolve, reject) => {
-    console()
+let laundry = function () {
+  return new Promise(function (resolve, reject) {
+    resolve(' laundry')
   })
-}
+};
+
+laundry().then(function (message) {
+  console.log('is finished' + message)
+});
