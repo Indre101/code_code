71474be7e6039cff9727f6701e09d6e4a67a0e4a
Indre Zygaityte <zygaityte.indre@gmail.com>
2019-01-14 22:04:01 +0100

    added all then statments and added messag, all promise result logs out

--- js/promise/promise-practise/depending-promises-cleaning.js
+++ js/promise/promise-practise/depending-promises-cleaning.js
@@ -1,25 +1,28 @@
-let laundry = function () {
-  return new Promise(function (resolve, reject) {
-    resolve('laundry ')
-  })
-};
+    let laundry = function () {
+      return new Promise(function (resolve, reject) {
+        resolve('laundry ')
+      })
+    };
 
-let dishes = function () {
-  return new Promise((resolve, reject) => {
-    resolve('dishes ')
-  })
-}
+    // message parameter can be names any word
+    // passed into first laundry function message read as undifiened
 
-let dusting = function () {
-  return new Promise((resolve, reject) => {
-    resolve('dusting')
-  })
-}
+    let dishes = function (message) {
+      return new Promise((resolve, reject) => {
+        resolve(message + 'dishes ')
+      })
+    }
 
-laundry().then((result) => {
-  return dishes(result)
-}).then((result) => {
-  return dusting(result)
-}).then((result) => {
-  console.log(result + ' finished')
-})
+    let dusting = function (message) {
+      return new Promise((resolve, reject) => {
+        resolve(message + 'dusting')
+      })
+    }
+    /// without message in parameters it prints out result of last Promise
+    laundry().then((result) => {
+      return dishes(result)
+    }).then((result) => {
+      return dusting(result)
+    }).then((result) => {
+      console.log(result + ' finished')
+    })
