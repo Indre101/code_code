40191748f9180b63ce5fda6db730c1dfc59bc4be
Indre Zygaityte <zygaityte.indre@gmail.com>
2019-01-15 19:18:15 +0100

    added check pattern on the sides of page

--- web-project/script.js
+++ web-project/script.js
@@ -4,16 +4,17 @@ const menuContent = document.getElementById('menuContent');
 // creating a menu that has a drop down list of options
 menuContent.hidden = true;
 
+
 buttonMenu.onmouseover = function () {
   buttonMenu.style.backgroundColor = '#75B1D7';
   menuContent.hidden = false;
-
 
   menuContent.onmouseover = function () {
     menuContent.hidden = false;
 
   }
 }
+
 buttonMenu.onmouseout = function () {
   buttonMenu.style.backgroundColor = '';
   menuContent.hidden = true;
@@ -22,3 +23,16 @@ buttonMenu.onmouseout = function () {
     menuContent.hidden = true;
   }
 }
+
+// colorful side functionality
+
+const colorful = document.getElementById('colorful');
+
+function randomColor(r, g, b) {
+
+  r = Math.floor(Math.random() * 255);
+  g = Math.floor(Math.random() * 255);
+  b = Math.floor(Math.random() * 255);
+  return `${r}${g}{b}`
+}
+console.log(randomColor(r, g, b));
