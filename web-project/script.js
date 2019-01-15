e07cdb5688d8c3b2a981111f4cd8592e229f7e96
Indre Zygaityte <zygaityte.indre@gmail.com>
2019-01-15 16:52:38 +0100

    added functional menu button and other pages html files

--- web-project/script.js
+++ web-project/script.js
@@ -1,19 +1,24 @@
 const buttonMenu = document.getElementById('menu');
 const menuContent = document.getElementById('menuContent');
 
+// creating a menu that has a drop down list of options
 menuContent.hidden = true;
 
 buttonMenu.onmouseover = function () {
   buttonMenu.style.backgroundColor = '#75B1D7';
   menuContent.hidden = false;
-  menuContent.position = 'absolute';
 
 
+  menuContent.onmouseover = function () {
+    menuContent.hidden = false;
 
+  }
 }
-
 buttonMenu.onmouseout = function () {
   buttonMenu.style.backgroundColor = '';
   menuContent.hidden = true;
 
+  menuContent.onmouseout = function () {
+    menuContent.hidden = true;
+  }
 }
