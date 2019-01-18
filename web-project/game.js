a678a313c9f0368fc823df2dcad4766717e49b90
Indre Zygaityte <zygaityte.indre@gmail.com>
2019-01-18 20:44:59 +0100

    assigned start button

--- web-project/game.js
+++ web-project/game.js
@@ -1,14 +1,33 @@
 let spiderman = 'spiderman.jpg'
 let building = 'building.jpg';
 let building1 = 'building1.jpg';
-
+let closed = 'door.jpg'
 
 let firstDoor = document.getElementById('firstDoor');
 let secondDoor = document.getElementById('secondDoor');
 let thirdDoor = document.getElementById('thirdDoor');
+let start = document.getElementById('start');
 
 
-firstDoor.onclick = function () {
 
-  firstDoor.src = building1;
-};
+function start() {
+
+  if (start.onclick) {
+
+    start.innerHTML = 'Good Luck';
+
+  } else {
+    firstDoor.src = closed;
+    secondDoor.src = closed;
+    thirdDoor.src = closed;
+  }
+
+}
+
+
+
+
+// firstDoor.onclick = function () {
+
+//   firstDoor.src = building1;
+// };
