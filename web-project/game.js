6f0a744993c50a7b96b842f4e00b081b42708b16
Indre Zygaityte <zygaityte.indre@gmail.com>
2019-01-18 19:58:16 +0100

    trying to make images of the door change

--- web-project/game.js
+++ web-project/game.js
@@ -1,1 +1,14 @@
-let spirderman = 'spiderman.jpeg';
+const spirderman = 'spiderman.jpg';
+const building1 = 'building1.jpg';
+const building = 'building.jpg';
+
+let firstDoor = document.getElementById('firstDoor');
+let secondDoor = document.getElementById('secondDoor');
+let thirdDoor = document.getElementById('thirdDoor');
+
+
+// firstDoor.onmouseover = function () {
+
+//   firstDoor.style.backgroundImage = building1;
+
+// };
